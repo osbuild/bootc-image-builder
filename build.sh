2e537d7f0c6e4c326253b0da6892fc112f64f7d5
Achilleas Koutsou <achilleas@koutsou.net>
2023-11-24 18:26:07 +0100

    build.sh: build binary from sources in repo

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 set -euo pipefail
 
-
-git clone --branch bifrost-image --depth 1 https://github.com/achilleas-k/images.git
-cd images
-go build ./cmd/osbuild-deploy-container
+cd odc
+go build -o ../bin/osbuild-deploy-container ./cmd/osbuild-deploy-container
