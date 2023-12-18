6c043b152b4386a8f8fcd4b93a50ac58d8fec9a3
Achilleas Koutsou <achilleas@koutsou.net>
2023-12-18 19:28:40 +0100

    cmd/bootc-image-builder: uploadAMI() helper function
    A new helper function that reads the command line flags and performs all
    the steps for uploading and registering an AMI from a local disk images.
    This is very similar to the uploadAMI() function in cmd/upload but they
    differ in the command line flags and error handling.

---
+++ bib/cmd/bootc-image-builder/cloud.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/osbuild/bootc-image-builder/bib/internal/uploader"
+	"github.com/spf13/pflag"
+)
+
+func uploadAMI(path string, flags *pflag.FlagSet) error {
+	region, err := flags.GetString("aws-region")
+	if err != nil {
+		return err
+	}
+	bucketName, err := flags.GetString("aws-bucket")
+	if err != nil {
+		return err
+	}
+	imageName, err := flags.GetString("aws-ami-name")
+	if err != nil {
+		return err
+	}
+
+	client, err := uploader.NewAWSClient(region)
+	if err != nil {
+		return err
+	}
+	return uploader.UploadAndRegister(client, path, bucketName, imageName)
+}
