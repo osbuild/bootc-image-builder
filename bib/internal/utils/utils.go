2f53edf31786d96bf6cb7116908f34dee3111c69
Colin Walters <walters@verbum.org>
2024-01-09 13:21:24 -0500

    utils: Simplify IsMountpoint
    Signed-off-by: Colin Walters <walters@verbum.org>

--- bib/internal/utils/utils.go
+++ bib/internal/utils/utils.go
@@ -11,10 +11,7 @@ import (
 
 // IsMountpoint checks if the target path is a mount point
 func IsMountpoint(path string) bool {
-	c := exec.Command("mountpoint", path)
-	c.Stderr = nil
-	c.Stdout = nil
-	return c.Run() == nil
+	return exec.Command("mountpoint", path).Run() == nil
 }
 
 // Synchronously invoke a command, propagating stdout and stderr
