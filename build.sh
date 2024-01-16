eeca163ec1a56df1768240e94802920ac81e9755
Colin Walters <walters@verbum.org>
2024-01-16 13:22:10 -0500

    build: Use tags to trim down containers/storage dependencies
    The default depends on btrfs, etc. that we don't need because
    ultimately we're just forking off skopeo anyways.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -euo pipefail
+# Keep this in sync with e.g. https://github.com/containers/podman/blob/2981262215f563461d449b9841741339f4d9a894/Makefile#L51
+# It turns off the esoteric containers-storage backends that add dependencies
+# on things like btrfs that we don't need.
+CONTAINERS_STORAGE_THIN_TAGS="containers_image_openpgp exclude_graphdriver_btrfs exclude_graphdriver_devicemapper exclude_graphdriver_overlay"
 
 cd bib
-go build -o ../bin/bootc-image-builder ./cmd/bootc-image-builder
+set -x
+go build -tags "${CONTAINERS_STORAGE_THIN_TAGS}" -o ../bin/bootc-image-builder ./cmd/bootc-image-builder
