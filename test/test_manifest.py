e8cdb8ac0c9ebdc94ef6954b94640140da12c662
Achilleas Koutsou <achilleas@koutsou.net>
2024-03-07 15:51:37 +0100

    test: use multi-image builds
    Add a new fixtures, images, that reuses the code from the image_type
    fixture.  Both the images and image_type fixture now call a new
    function, build_images, that handles either building new images or
    returning cached builds.
    
    When one or more disk images are requested, the build_images() function
    uses the new multi-build feature to build all disk image types and
    returns the artifacts that were requested.
    
    A new test, test_multi_build_request(), ensures that the returned image
    artifacts exist and match the expected file names.
    
    ISO tests are unaffected by these changes since they can only built
    separately.

--- test/test_manifest.py
+++ test/test_manifest.py
@@ -5,7 +5,6 @@ import pytest
 
 import testutil
 
-
 if not testutil.has_executable("podman"):
     pytest.skip("no podman, skipping integration tests that required podman", allow_module_level=True)
 
@@ -13,9 +12,9 @@ from containerbuild import build_container_fixture  # noqa: F401
 from testcases import gen_testcases
 
 
-@pytest.mark.parametrize("image_type", gen_testcases("manifest"))
-def test_manifest_smoke(build_container, image_type):
-    container_ref = image_type.split(",")[0]
+@pytest.mark.parametrize("images", gen_testcases("manifest"))
+def test_manifest_smoke(build_container, images):
+    container_ref = images.split(",")[0]
 
     output = subprocess.check_output([
         "podman", "run", "--rm",
