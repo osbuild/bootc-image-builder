610f703d2cb2d6e973e7e07e80fb96a106f0bd37
Ondřej Budai <ondrej@budai.cz>
2024-04-17 15:25:35 +0200

    bib: add a library for getting information about the source container
    For the installer, we need to know some information about the container.
    This commit introduces a small library for this purpose. It will be used
    by the subsequent commits.

---
+++ bib/internal/source/source_test.go
@@ -0,0 +1,85 @@
+package source
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func writeOSRelease(root, id, versionID, name, platformID string) error {
+	err := os.MkdirAll(path.Join(root, "etc"), 0755)
+	if err != nil {
+		return err
+	}
+
+	var buf string
+	if id != "" {
+		buf += "ID=" + id + "\n"
+	}
+	if versionID != "" {
+		buf += "VERSION_ID=" + versionID + "\n"
+	}
+	if name != "" {
+		buf += "NAME=" + name + "\n"
+	}
+	if platformID != "" {
+		buf += "PLATFORM_ID=" + platformID + "\n"
+	}
+
+	return os.WriteFile(path.Join(root, "etc/os-release"), []byte(buf), 0644)
+}
+
+func createBootupdEFI(root, uefiVendor string) error {
+	err := os.MkdirAll(path.Join(root, "usr/lib/bootupd/updates/EFI/BOOT"), 0755)
+	if err != nil {
+		return err
+	}
+	return os.Mkdir(path.Join(root, "usr/lib/bootupd/updates/EFI", uefiVendor), 0755)
+}
+
+func TestLoadInfo(t *testing.T) {
+	cases := []struct {
+		desc       string
+		id         string
+		versionID  string
+		name       string
+		uefiVendor string
+		platformID string
+		errorStr   string
+	}{
+		{"happy", "fedora", "40", "Fedora Linux", "fedora", "platform:f40", ""},
+		{"happy-no-uefi", "fedora", "40", "Fedora Linux", "", "platform:f40", ""},
+		{"sad-no-id", "", "40", "Fedora Linux", "fedora", "platform:f40", "missing ID in os-release"},
+		{"sad-no-id", "fedora", "", "Fedora Linux", "fedora", "platform:f40", "missing VERSION_ID in os-release"},
+		{"sad-no-id", "fedora", "40", "", "fedora", "platform:f40", "missing NAME in os-release"},
+		{"sad-no-id", "fedora", "40", "Fedora Linux", "fedora", "", "missing PLATFORM_ID in os-release"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.desc, func(t *testing.T) {
+			root := t.TempDir()
+			require.NoError(t, writeOSRelease(root, c.id, c.versionID, c.name, c.platformID))
+			if c.uefiVendor != "" {
+				require.NoError(t, createBootupdEFI(root, c.uefiVendor))
+
+			}
+
+			info, err := LoadInfo(root)
+
+			if c.errorStr != "" {
+				require.Equal(t, c.errorStr, err.Error())
+				return
+			}
+			require.NoError(t, err)
+			assert.Equal(t, c.id, info.ID)
+			assert.Equal(t, c.versionID, info.VersionID)
+			assert.Equal(t, c.name, info.Name)
+			assert.Equal(t, c.uefiVendor, info.UEFIVendor)
+			assert.Equal(t, c.platformID, info.PlatformID)
+
+		})
+	}
+}
