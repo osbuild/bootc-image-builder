ebd1f215c66a584d6e0c859346541f70423d6f5c
Ondřej Budai <ondrej@budai.cz>
2024-04-17 15:25:35 +0200

    bib: add a library for getting information about the source container
    For the installer, we need to know some information about the container.
    This commit introduces a small library for this purpose. It will be used
    by the subsequent commits.

---
+++ bib/internal/source/source.go
@@ -0,0 +1,73 @@
+package source
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/osbuild/images/pkg/distro"
+	"github.com/sirupsen/logrus"
+)
+
+type Info struct {
+	ID         string
+	VersionID  string
+	Name       string
+	UEFIVendor string
+	PlatformID string
+}
+
+func validateOSRelease(osrelease map[string]string) error {
+	for _, key := range []string{"ID", "VERSION_ID", "NAME", "PLATFORM_ID"} {
+		if _, ok := osrelease[key]; !ok {
+			return fmt.Errorf("missing %s in os-release", key)
+		}
+	}
+	return nil
+}
+
+func uefiVendor(root string) (string, error) {
+	bootupdEfiDir := path.Join(root, "usr/lib/bootupd/updates/EFI")
+	l, err := os.ReadDir(bootupdEfiDir)
+	if err != nil {
+		return "", fmt.Errorf("cannot read bootupd EFI directory %s: %w", bootupdEfiDir, err)
+	}
+
+	// best-effort search: return the first directory that's not "BOOT"
+	for _, entry := range l {
+		if !entry.IsDir() {
+			continue
+		}
+
+		if entry.Name() == "BOOT" {
+			continue
+		}
+
+		return entry.Name(), nil
+	}
+
+	return "", fmt.Errorf("cannot find UEFI vendor in %s", bootupdEfiDir)
+}
+
+func LoadInfo(root string) (*Info, error) {
+	osrelease, err := distro.ReadOSReleaseFromTree(root)
+	if err != nil {
+		return nil, err
+	}
+	if err := validateOSRelease(osrelease); err != nil {
+		return nil, err
+	}
+
+	vendor, err := uefiVendor(root)
+	if err != nil {
+		logrus.Debugf("cannot read UEFI vendor: %v, setting it to none", err)
+	}
+
+	return &Info{
+		ID:         osrelease["ID"],
+		VersionID:  osrelease["VERSION_ID"],
+		Name:       osrelease["NAME"],
+		PlatformID: osrelease["PLATFORM_ID"],
+		UEFIVendor: vendor,
+	}, nil
+}
