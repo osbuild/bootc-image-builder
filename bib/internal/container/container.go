09a64ff2a3687a19ea987681a0775908707fdddf
Ondřej Budai <ondrej@budai.cz>
2024-04-19 13:53:11 +0200

    internal/container: refactor command calling
    Michael pointed out in the review that exec.Command().Output or
    CombinedOutput is more convenient and easier to read. Let's use these
    in the container library.

--- bib/internal/container/container.go
+++ bib/internal/container/container.go
@@ -1,7 +1,6 @@
 package container
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -41,18 +40,16 @@ func New(ref string) (c *Container, err error) {
 
 	args = append(args, ref, "infinity")
 
-	cmd := exec.Command("podman", args...)
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("running %s container failed: %w\nstderr:\n%s", ref, err, stderr.String())
+	output, err := exec.Command("podman", args...).Output()
+	if err != nil {
+		if e, ok := err.(*exec.ExitError); ok {
+			return nil, fmt.Errorf("running %s container failed: %w\nstderr:\n%s", ref, e, e.Stderr)
+		}
+		return nil, fmt.Errorf("running %s container failed with generic error: %w", ref, err)
 	}
 
 	c = &Container{}
-	c.id = strings.TrimSpace(stdout.String())
+	c.id = strings.TrimSpace(string(output))
 	// Ensure that the container is stopped when this function errors
 	defer func() {
 		if err != nil {
@@ -63,16 +60,14 @@ func New(ref string) (c *Container, err error) {
 		}
 	}()
 
-	stdout.Reset()
-	stderr.Reset()
-	cmd = exec.Command("podman", "mount", c.id)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	if runErr := cmd.Run(); runErr != nil {
-		err = fmt.Errorf("mounting %s container (%s) failed: %w\nstderr:\n%s", c.id, ref, runErr, stderr.String())
-		return
+	output, err = exec.Command("podman", "mount", c.id).Output()
+	if err != nil {
+		if err, ok := err.(*exec.ExitError); ok {
+			return nil, fmt.Errorf("mounting %s container failed: %w\nstderr:\n%s", ref, err, err.Stderr)
+		}
+		return nil, fmt.Errorf("mounting %s container failed with generic error: %w", ref, err)
 	}
-	c.root = strings.TrimSpace(stdout.String())
+	c.root = strings.TrimSpace(string(output))
 
 	return
 }
@@ -80,12 +75,8 @@ func New(ref string) (c *Container, err error) {
 // Stop stops the container. Since New() creates a container with --rm, this
 // removes the container as well.
 func (c *Container) Stop() error {
-	cmd := exec.Command("podman", "stop", c.id)
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
-
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("stopping %s container failed: %w\nstderr:\n%s", c.id, err, stderr.String())
+	if output, err := exec.Command("podman", "stop", c.id).CombinedOutput(); err != nil {
+		return fmt.Errorf("stopping %s container failed: %w\noutput:\n%s", c.id, err, output)
 	}
 
 	return nil
@@ -98,24 +89,21 @@ func (c *Container) Root() string {
 
 // Reads a file from the container
 func (c *Container) ReadFile(path string) ([]byte, error) {
-	cmd := exec.Command("podman", "exec", c.id, "cat", path)
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("reading %s from %s container failed: %w\nstderr:\n%s", path, c.id, err, stderr.String())
+	output, err := exec.Command("podman", "exec", c.id, "cat", path).Output()
+	if err != nil {
+		if err, ok := err.(*exec.ExitError); ok {
+			return nil, fmt.Errorf("reading %s from %s container failed: %w\nstderr:\n%s", path, c.id, err, err.Stderr)
+		}
+		return nil, fmt.Errorf("reading %s from %s container failed with generic error: %w", path, c.id, err)
 	}
 
-	return stdout.Bytes(), nil
+	return output, nil
 }
 
 // CopyInto copies a file into the container.
 func (c *Container) CopyInto(src, dest string) error {
-	cmd := exec.Command("podman", "cp", src, c.id+":"+dest)
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("copying %s into %s container failed: %w\nstderr:\n%s", src, c.id, err, stderr.String())
+	if output, err := exec.Command("podman", "cp", src, c.id+":"+dest).CombinedOutput(); err != nil {
+		return fmt.Errorf("copying %s into %s container failed: %w\noutput:\n%s", src, c.id, err, output)
 	}
 
 	return nil
@@ -131,11 +119,8 @@ func (c *Container) ExecArgv() []string {
 //
 // The implementation is simple: We just run plain `dnf` in the container.
 func (c *Container) InitDNF() error {
-	var stderr bytes.Buffer
-	cmd := exec.Command("podman", "exec", c.id, "dnf")
-	cmd.Stderr = &stderr
-	if runErr := cmd.Run(); runErr != nil {
-		return fmt.Errorf("initializing dnf in %s container failed: %w\nstderr:\n%s", c.id, runErr, stderr.String())
+	if output, err := exec.Command("podman", "exec", c.id, "dnf").CombinedOutput(); err != nil {
+		return fmt.Errorf("initializing dnf in %s container failed: %w\noutput:\n%s", c.id, err, output)
 	}
 
 	return nil
