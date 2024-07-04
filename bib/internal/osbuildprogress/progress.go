31b653c934c7db33e17bcd772731586de1337781
Michael Vogt <michael.vogt@gmail.com>
2024-07-04 17:54:11 +0200

    osbuildprogress: switch to `chegggaaa/pb/v3` for progress
    Switch the progress output to `chegggaaa/pb/v3` and tweak the
    progress. Still open questions about the message output and
    error handling.

--- bib/internal/osbuildprogress/progress.go
+++ bib/internal/osbuildprogress/progress.go
@@ -10,6 +10,8 @@ import (
 	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 type OsbuildJsonProgress struct {
@@ -27,14 +29,14 @@ type OsbuildJsonProgress struct {
 	} `json:"context"`
 	Progress struct {
 		Name  string `json:"name"`
-		Total int    `json:"total"`
-		Done  int    `json:"done"`
+		Total int64  `json:"total"`
+		Done  int64  `json:"done"`
 		// XXX: there are currently only two levels but it should be
 		// deeper nested in theory
 		SubProgress struct {
 			Name  string `json:"name"`
-			Total int    `json:"total"`
-			Done  int    `json:"done"`
+			Total int64  `json:"total"`
+			Done  int64  `json:"done"`
 			// XXX: in theory this could be more nested but it's not
 
 		} `json:"progress"`
@@ -43,7 +45,7 @@ type OsbuildJsonProgress struct {
 	Message string `json:"message"`
 }
 
-func scanJsonSeq(r io.Reader, ch chan OsbuildJsonProgress) {
+func scanJsonSeq(r io.Reader, ch chan OsbuildJsonProgress, errCh chan error) {
 	var progress OsbuildJsonProgress
 
 	scanner := bufio.NewScanner(r)
@@ -53,33 +55,48 @@ func scanJsonSeq(r io.Reader, ch chan OsbuildJsonProgress) {
 		line = bytes.Trim(line, "\x1e")
 		if err := json.Unmarshal(line, &progress); err != nil {
 			// XXX: provide an invalid lines chan
-			fmt.Fprintf(os.Stderr, "json decode err for line %q: %v\n", line, err)
+			errCh <- err
 			continue
 		}
 		ch <- progress
 	}
 	if err := scanner.Err(); err != nil && err != io.EOF {
-		// log error here
+		errCh <- err
 	}
 }
 
 func AttachProgress(r io.Reader, w io.Writer) {
 	var progress OsbuildJsonProgress
-	spinner := []string{"|", "/", "-", "\\"}
-	i := 0
 
 	ch := make(chan OsbuildJsonProgress)
-	go scanJsonSeq(r, ch)
+	errCh := make(chan error)
+	go scanJsonSeq(r, ch, errCh)
 
-	mainProgress := "unknown"
-	subProgress := ""
-	message := "-"
+	lastMessage := "-"
+
+	spinnerPb := pb.New(0)
+	spinnerPb.SetTemplate(`Building [{{ (cycle . "|" "/" "-" "\\") }}]`)
+	mainPb := pb.New(0)
+	progressBarTmplFmt := `[{{ counters . }}] %s: {{ string . "prefix" }} {{ bar .}} {{ percent . }}`
+	mainPb.SetTemplateString(fmt.Sprintf(progressBarTmplFmt, "step"))
+	subPb := pb.New(0)
+	subPb.SetTemplateString(fmt.Sprintf(progressBarTmplFmt, "module"))
+	msgPb := pb.New(0)
+	msgPb.SetTemplate(`last msg: {{ string . "msg" }}`)
+
+	pool, err := pb.StartPool(spinnerPb, mainPb, subPb, msgPb)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "progress failed: %v\n", err)
+		return
+	}
 
 	contextMap := map[string]string{}
 
-	fmt.Fprintf(w, "\n")
 	for {
 		select {
+		case err := <-errCh:
+			fmt.Fprintf(os.Stderr, "error: %v", err)
+			break
 		case progress = <-ch:
 			id := progress.Context.Pipeline.ID
 			pipelineName := contextMap[id]
@@ -96,54 +113,38 @@ func AttachProgress(r io.Reader, w io.Writer) {
 			}
 
 			if progress.Progress.Total > 0 {
-				mainProgress = fmt.Sprintf("step: %v [%v/%v]", pipelineName, progress.Progress.Done+1, progress.Progress.Total+1)
+				mainPb.SetTotal(progress.Progress.Total + 1)
+				mainPb.SetCurrent(progress.Progress.Done + 1)
+				mainPb.Set("prefix", pipelineName)
 			}
 			// XXX: use context instead of name here too
 			if progress.Progress.SubProgress.Total > 0 {
-				subProgress = fmt.Sprintf("%v [%v/%v]", stageName, progress.Progress.SubProgress.Done+1, progress.Progress.SubProgress.Total+1)
+				subPb.SetTotal(progress.Progress.SubProgress.Total + 1)
+				subPb.SetCurrent(progress.Progress.SubProgress.Done + 1)
+				subPb.Set("prefix", strings.TrimPrefix(stageName, "org.osbuild."))
 			}
 
 			// todo: make message more structured in osbuild?
 			// message from the stages themselfs are very noisy
 			// best not to show to the user (only for failures)
 			if progress.Context.Origin == "osbuild.monitor" {
-				message = progress.Message
+				lastMessage = progress.Message
 			}
-			//message = strings.TrimSpace(strings.SplitN(progress.Message, "\n", 2)[0])
-			// todo: fix in osbuild?
 			/*
-				l := strings.SplitN(message, ":", 2)
+				// todo: fix in osbuild?
+				lastMessage = strings.TrimSpace(strings.SplitN(progress.Message, "\n", 2)[0])
+				l := strings.SplitN(lastMessage, ":", 2)
 				if len(l) > 1 {
-					message = strings.TrimSpace(l[1])
+					lastMessage = strings.TrimSpace(l[1])
 				}
 			*/
-			if len(message) > 60 {
-				message = message[:60] + "..."
-			}
+			msgPb.Set("msg", lastMessage)
+
 		case <-time.After(200 * time.Millisecond):
 			// nothing
 		}
-
-		// XXX: use real progressbar *or* use helper to get terminal
-		// size for proper length checks etc
-		//
-		// poor man progress, we need multiple progress bars and
-		// a message that keeps getting updated (or maybe not the
-		// message)
-		fmt.Fprintf(w, "\x1b[2KBuilding [%s]\n", spinner[i])
-		fmt.Fprintf(w, "\x1b[2Kstep   : %s\n", mainProgress)
-		if subProgress != "" {
-			fmt.Fprintf(w, "\x1b[2Kmodule : %s\n", strings.TrimPrefix(subProgress, "org.osbuild."))
-		}
-		fmt.Fprintf(w, "\x1b[3Kmessage: %s\n", message)
-		if subProgress != "" {
-			fmt.Fprintf(w, "\x1b[%dA", 4)
-		} else {
-			fmt.Fprintf(w, "\x1b[%dA", 3)
-		}
-		// spin
-		i = (i + 1) % len(spinner)
 	}
+	pool.Stop()
 }
 
 // XXX: merge back into images/pkg/osbuild/osbuild-exec.go(?)
@@ -172,5 +173,6 @@ func RunOSBuild(manifest []byte, store, outputDirectory string, exports, extraEn
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("error starting osbuild: %v", err)
 	}
+	// XXX: add WaitGroup
 	return cmd.Wait()
 }
