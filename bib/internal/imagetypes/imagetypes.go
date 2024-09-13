ad6d3a3092e0b5a4d12110f3ab070b53a0784ee9
Michael Vogt <michael.vogt@gmail.com>
2024-09-13 16:52:17 +0200

    internal: add new `imagetypes` package to centralize new img types
    When adding new image types like `vhd` or the coming `gcp` I
    noticed that we currently need to modify two different places.
    
    This package consolidates all knowledge about image types, their
    exports and if they are ISO or disk into a single package. This
    should make it easier to expand the supported image types.

---
+++ bib/internal/imagetypes/imagetypes.go
@@ -0,0 +1,87 @@
+package imagetypes
+
+import (
+	"fmt"
+	"slices"
+	"sort"
+	"strings"
+)
+
+type imageType struct {
+	Export string
+	ISO    bool
+}
+
+var supportedImageTypes = map[string]imageType{
+	"ami":          imageType{Export: "image"},
+	"qcow2":        imageType{Export: "qcow2"},
+	"raw":          imageType{Export: "image"},
+	"vmdk":         imageType{Export: "vmdk"},
+	"vhd":          imageType{Export: "vpc"},
+	"anaconda-iso": imageType{Export: "anaconda-iso", ISO: true},
+	"iso":          imageType{Export: "iso", ISO: true},
+}
+
+// Available() returns a comma-separated list of supported image types
+func Available() string {
+	keys := make([]string, 0, len(supportedImageTypes))
+	for k := range supportedImageTypes {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	return strings.Join(keys, ", ")
+}
+
+// ImageTypes contains the image types that are requested to be build
+type ImageTypes []string
+
+// New takes image type names as input and returns a ImageTypes
+// object or an error if the image types are invalid.
+//
+// Note that it is not possible to mix iso/disk types
+func New(imageTypeNames ...string) (ImageTypes, error) {
+	if len(imageTypeNames) == 0 {
+		return nil, fmt.Errorf("cannot use an empty array as a build request")
+	}
+
+	var ISOs, disks int
+	for _, name := range imageTypeNames {
+		imgType, ok := supportedImageTypes[name]
+		if !ok {
+			return nil, fmt.Errorf("unsupported image type %q, valid types are %s", name, Available())
+		}
+		if imgType.ISO {
+			ISOs++
+		} else {
+			disks++
+		}
+	}
+	if ISOs > 0 && disks > 0 {
+		return nil, fmt.Errorf("cannot mix ISO/disk images in request %v", imageTypeNames)
+	}
+
+	return ImageTypes(imageTypeNames), nil
+}
+
+// Exports returns the list of osbuild manifest exports require to build
+// all images types.
+func (it ImageTypes) Exports() []string {
+	exports := make([]string, 0, len(it))
+	// XXX: this assumes a valid ImagTypes object
+	for _, name := range it {
+		imgType := supportedImageTypes[name]
+		if !slices.Contains(exports, imgType.Export) {
+			exports = append(exports, imgType.Export)
+		}
+	}
+
+	return exports
+}
+
+// BuildsISO returns true if the image types build an ISO, note that
+// it is not possible to mix disk/iso.
+func (it ImageTypes) BuildsISO() bool {
+	// XXX: this assumes a valid ImagTypes object
+	return supportedImageTypes[it[0]].ISO
+}
---
+++ bib/internal/imagetypes/imagetypes_test.go
@@ -0,0 +1,90 @@
+package imagetypes_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/osbuild/bootc-image-builder/bib/internal/imagetypes"
+)
+
+type testCase struct {
+	imageTypes      []string
+	expectedExports []string
+	expectISO       bool
+	expectedErr     error
+}
+
+func TestImageTypes(t *testing.T) {
+	testCases := map[string]testCase{
+		"qcow-disk": {
+			imageTypes:      []string{"qcow2"},
+			expectedExports: []string{"qcow2"},
+			expectISO:       false,
+		},
+		"ami-disk": {
+			imageTypes:      []string{"ami"},
+			expectedExports: []string{"image"},
+			expectISO:       false,
+		},
+		"qcow-ami-disk": {
+			imageTypes:      []string{"qcow2", "ami"},
+			expectedExports: []string{"qcow2", "image"},
+			expectISO:       false,
+		},
+		"ami-raw": {
+			imageTypes:      []string{"ami", "raw"},
+			expectedExports: []string{"image"},
+			expectISO:       false,
+		},
+		"all-disk": {
+			imageTypes:      []string{"ami", "raw", "vmdk", "qcow2"},
+			expectedExports: []string{"image", "vmdk", "qcow2"},
+			expectISO:       false,
+		},
+		"iso": {
+			imageTypes:      []string{"iso"},
+			expectedExports: []string{"iso"},
+			expectISO:       true,
+		},
+		"anaconda": {
+			imageTypes:      []string{"anaconda-iso"},
+			expectedExports: []string{"anaconda-iso"},
+			expectISO:       true,
+		},
+		"bad-mix": {
+			imageTypes:  []string{"vmdk", "anaconda-iso"},
+			expectedErr: errors.New("cannot mix ISO/disk images in request [vmdk anaconda-iso]"),
+		},
+		"bad-mix-part-2": {
+			imageTypes:  []string{"ami", "iso"},
+			expectedErr: errors.New("cannot mix ISO/disk images in request [ami iso]"),
+		},
+		"bad-image-type": {
+			imageTypes:  []string{"bad"},
+			expectedErr: errors.New(`unsupported image type "bad", valid types are ami, anaconda-iso, iso, qcow2, raw, vhd, vmdk`),
+		},
+		"bad-in-good": {
+			imageTypes:  []string{"ami", "raw", "vmdk", "qcow2", "something-else-what-is-this"},
+			expectedErr: errors.New(`unsupported image type "something-else-what-is-this", valid types are ami, anaconda-iso, iso, qcow2, raw, vhd, vmdk`),
+		},
+		"all-bad": {
+			imageTypes:  []string{"bad1", "bad2", "bad3", "bad4", "bad5", "bad42"},
+			expectedErr: errors.New(`unsupported image type "bad1", valid types are ami, anaconda-iso, iso, qcow2, raw, vhd, vmdk`),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			it, err := imagetypes.New(tc.imageTypes...)
+			if tc.expectedErr != nil {
+				assert.Equal(t, err, tc.expectedErr)
+			} else {
+				assert.Equal(t, it.Exports(), tc.expectedExports)
+				assert.Equal(t, it.BuildsISO(), tc.expectISO)
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
