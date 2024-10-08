osbuild/bootc-image-builder

commit cfe9c68b06d346f67c307ce87283646106c6c12a

container: Fix the Stop function for containers
Some podman versions remove the image when stopped, some don't.
Use podman rm --ignore to ignore this and continue.

Related: RHEL-50632

bib/internal/container/container.go
@@ -83,9 +83,9 @@ func (c *Container) Stop() error {
 	if output, err := exec.Command("podman", "stop", c.id).CombinedOutput(); err != nil {
 		return fmt.Errorf("stopping %s container failed: %w\noutput:\n%s", c.id, err, output)
 	}
-	// when the container is stopped by podman it will not honor the "--rm"
-	// that was passed in `New()` so manually remove the container here.
-	if output, err := exec.Command("podman", "rm", c.id).CombinedOutput(); err != nil {
+	// when the container is stopped by podman it may not honor the "--rm"
+	// that was passed in `New()` so manually remove the container here if it is still available
+	if output, err := exec.Command("podman", "rm", "--ignore", c.id).CombinedOutput(); err != nil {
 		return fmt.Errorf("removing %s container failed: %w\noutput:\n%s", c.id, err, output)
 	}
 

commit a741b6b2a16dc597e3d293549746e5d11c3614a1

container: copy RHSM secrets into the ephemeral container
We recently ran into a regression in bib where it can no longer
access subscribed content. This is caused by commit cfe9c68. This
commit changed the way osbuild-dnf-json was run, instead of inside
the container it runs now outside with the dnfjson.SetRootDir()
mount of the container. But this means that the secrets mapped
via a volume from `-v /run/secrets:/run/secrets` are no longer
accessible because this dir is only available inside the mount
namespace of the container but not available from the outside.

Instead of the mount this commit uses a rather blunt approach
and just copies the content of /run/secrets into the container.
This is (very) ugly but it unbreaks the functionality.

bib/internal/container/container.go
@@ -12,6 +12,11 @@ import (
 	"github.com/osbuild/bootc-image-builder/bib/internal/util"
 )
 
+var (
+	secretDirSrc = "/run/secrets"
+	secretDirDst = "/run/secrets"
+)
+
 // Container is a simpler wrapper around a running podman container.
 // This type isn't meant as a general-purpose container management tool, but
 // as an opinonated library for bootc-image-builder.
@@ -26,9 +31,6 @@ type Container struct {
 // - --net host is used to make networking work in a nested container
 // - /run/secrets is mounted from the host to make sure RHSM credentials are available
 func New(ref string) (*Container, error) {
-	const secretDir = "/run/secrets"
-	secretVolume := fmt.Sprintf("%s:%s", secretDir, secretDir)
-
 	args := []string{
 		"run",
 		"--rm",
@@ -36,11 +38,6 @@ func New(ref string) (*Container, error) {
 		"--detach",
 		"--net", "host", // Networking in a nested container doesn't work without re-using this container's network
 		"--entrypoint", "sleep", // The entrypoint might be arbitrary, so let's just override it with sleep, we don't want to run anything
-	}
-
-	// Re-mount the secret directory if it exists
-	if _, err := os.Stat(secretDir); err == nil {
-		args = append(args, "--volume", secretVolume)
 	}
 
 	args = append(args, ref, "infinity")
@@ -73,6 +70,14 @@ func New(ref string) (*Container, error) {
 		return nil, fmt.Errorf("mounting %s container failed with generic error: %w", ref, err)
 	}
 	c.root = strings.TrimSpace(string(output))
+
+	// XXX: fugly
+	// Copy the secrets if they exist, we cannot just mount them
+	// inside the container because we run "dnf" via
+	// "SetRootDir()" which will not have a mounted /run
+	if _, err := os.Stat(secretDirSrc); err == nil {
+		c.CopyInto(secretDirSrc, secretDirDst)
+	}
 
 	return c, err
 }

bib/internal/container/container_test.go
@@ -80,6 +80,49 @@ func TestNew(t *testing.T) {
 	assert.Contains(t, string(osRelease), `ID="rhel"`)
 }
 
+func makeFakeRunSecrets(t *testing.T) string {
+	fakeSecretsRoot := filepath.Join(t.TempDir(), "/run/secrets")
+	fakeSecretsFile := filepath.Join(fakeSecretsRoot, "/etc-pki-entitlement/12345.pem")
+	err := os.MkdirAll(filepath.Dir(fakeSecretsFile), 0700)
+	require.NoError(t, err)
+	err = os.WriteFile(filepath.Join(fakeSecretsFile), []byte("very-secret"), 0600)
+	require.NoError(t, err)
+
+	restore := MockSecretDirSrc(fakeSecretsRoot)
+	t.Cleanup(restore)
+
+	return fakeSecretsRoot
+}
+
+func TestSecretsAvailableInsideContainer(t *testing.T) {
+	if os.Geteuid() != 0 {
+		t.Skip("skipping test; not running as root")
+	}
+
+	// fake /run/secrets as we would have it when running bib in a
+	// subscribed machine
+	makeFakeRunSecrets(t)
+
+	cnt, err := New(testingImage)
+	require.NoError(t, err)
+	defer cnt.Stop()
+
+	// ensure secrets are available when running "cat" inside the container
+	secretContent, err := cnt.ReadFile("/run/secrets/etc-pki-entitlement/12345.pem")
+	require.NoError(t, err)
+	assert.Equal(t, "very-secret", string(secretContent))
+
+	// ensure other /run content is available still
+	_, err = cnt.ReadFile("/run/motd")
+	require.NoError(t, err)
+
+	// ensure secrets are available when accessing the container root
+	root := cnt.Root()
+	secretContent, err = os.ReadFile(filepath.Join(root, "/run/secrets/etc-pki-entitlement/12345.pem"))
+	require.NoError(t, err)
+	assert.Equal(t, "very-secret", string(secretContent))
+}
+
 func TestReadFile(t *testing.T) {
 	if os.Geteuid() != 0 {
 		t.Skip("skipping test; not running as root")

bib/internal/container/export_test.go
@@ -1,0 +1,9 @@
+package container
+
+func MockSecretDirSrc(new string) (restore func()) {
+	saved := secretDirSrc
+	secretDirSrc = new
+	return func() {
+		secretDirSrc = saved
+	}
+}