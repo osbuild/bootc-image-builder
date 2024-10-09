a29239daa36ff09c27f938a2c0967e8c91f38a5c
Michael Vogt <michael.vogt@gmail.com>
2024-10-09 15:50:09 +0200

    bib: add test to ensure InitDNF() works with subscribed content
    This commit adds a test that ensures that InitDNF() results in
    triggering the dnf plugin that updates the subscriptions.

--- bib/internal/container/container_test.go
+++ bib/internal/container/container_test.go
@@ -8,13 +8,17 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-const testingImage = "registry.access.redhat.com/ubi9-micro:latest"
+const (
+	testingImage    = "registry.access.redhat.com/ubi9-micro:latest"
+	dnfTestingImage = "registry.access.redhat.com/ubi9:latest"
+)
 
 type containerInfo struct {
 	State string `json:"State"`
@@ -170,4 +174,53 @@ echo '%s'
 		_, err := cnt.DefaultRootfsType()
 		assert.ErrorContains(t, err, "unsupported root filesystem type: ext1, supported: ")
 	}
+}
+
+func subscribeMachine(t *testing.T) (restore func()) {
+	if _, err := exec.LookPath("subscription-manager"); err != nil {
+		t.Skip("no subscription-manager found")
+		return func() {}
+	}
+
+	matches, err := filepath.Glob("/etc/pki/entitlement/*.pem")
+	if err == nil && len(matches) > 0 {
+		return func() {}
+	}
+
+	rhsmOrg := os.Getenv("RHSM_ORG")
+	rhsmActivationKey := os.Getenv("RHSM_ACTIVATION_KEY")
+	if rhsmOrg == "" || rhsmActivationKey == "" {
+		t.Skip("no RHSM_{ORG,ACTIVATION_KEY} env vars found")
+		return func() {}
+	}
+
+	err = exec.Command("subscription-manager", "register",
+		"--org", rhsmOrg,
+		"--activationkey", rhsmActivationKey).Run()
+	require.NoError(t, err)
+
+	return func() {
+		exec.Command("subscription-manager", "unregister").Run()
+	}
+}
+
+func TestDNFInitGivesAccessToSubscribedContent(t *testing.T) {
+	if os.Geteuid() != 0 {
+		t.Skip("skipping test; not running as root")
+	}
+	if runtime.GOARCH != "amd64" {
+		t.Skip("skipping test; only runs on x86_64")
+	}
+
+	restore := subscribeMachine(t)
+	defer restore()
+
+	cnt, err := New(dnfTestingImage)
+	require.NoError(t, err)
+	err = cnt.InitDNF()
+	require.NoError(t, err)
+
+	content, err := cnt.ReadFile("/etc/yum.repos.d/redhat.repo")
+	require.NoError(t, err)
+	assert.Contains(t, string(content), "rhel-9-for-x86_64-baseos-rpms")
 }
