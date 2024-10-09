1ccd370d7c5206964657a3a432c73024211f3e25
Miguel Martín <mmartinv@redhat.com>
2024-10-09 08:27:59 +0200

    test: add a test to verify libosinfo detection
    Add a test to verify the anaconda ISO label is
    properly detected by libosinfo.
    
    Signed-off-by: Miguel Martín <mmartinv@redhat.com>

--- test/testcases.py
+++ test/testcases.py
@@ -41,12 +41,14 @@ class TestCase:
 class TestCaseFedora(TestCase):
     container_ref: str = "quay.io/fedora/fedora-bootc:40"
     rootfs: str = "btrfs"
+    osinfo_template: str = "Fedora Server 40 ({arch})"
 
 
 @dataclasses.dataclass
 class TestCaseFedora42(TestCase):
     container_ref: str = "quay.io/fedora/fedora-bootc:42"
     rootfs: str = "btrfs"
+    osinfo_template: str = "Fedora Server 42 ({arch})"
 
 
 @dataclasses.dataclass
@@ -54,6 +56,7 @@ class TestCaseCentos(TestCase):
     container_ref: str = os.getenv(
         "BIB_TEST_BOOTC_CONTAINER_TAG",
         "quay.io/centos-bootc/centos-bootc:stream9")
+    osinfo_template: str = "CentOS Stream 9 ({arch})"
 
 
 def gen_testcases(what):  # pylint: disable=too-many-return-statements
