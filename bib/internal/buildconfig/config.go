6ee0f6ef24312f068be2d44a9435d6263ea2945d
Michael Vogt <michael.vogt@gmail.com>
2025-03-20 16:13:08 +0100

    Revert "buildconfig: disable tests for undecoded keys for now"
    This reverts the relevant parts of commit
    f2ecdecc5f303fc88f10db96d091ca93738a3567.

--- bib/internal/buildconfig/config.go
+++ bib/internal/buildconfig/config.go
@@ -59,9 +59,13 @@ func decodeTomlBuildConfig(r io.Reader, what string) (*BuildConfig, error) {
 	dec := toml.NewDecoder(r)
 
 	var conf BuildConfig
-	_, err := dec.Decode(&conf)
+	metadata, err := dec.Decode(&conf)
 	if err != nil {
 		return nil, fmt.Errorf("cannot decode %q: %w", what, err)
+	}
+
+	if len(metadata.Undecoded()) > 0 {
+		return nil, fmt.Errorf("cannot decode %q: unknown keys found: %v", what, metadata.Undecoded())
 	}
 
 	return &conf, nil
