osbuild/bootc-image-builder

commit 2da427a5651ccdc25e4a631614b7d3b9a9c541e9

upload: update for awscloud API change

bib/cmd/upload/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	"github.com/osbuild/images/pkg/arch"
 	"github.com/osbuild/images/pkg/cloud/awscloud"
 )
 
@@ -29,9 +30,11 @@ func uploadAMI(cmd *cobra.Command, args []string) {
 	check(err)
 	imageName, err := flags.GetString("ami-name")
 	check(err)
-	targetArch, err := flags.GetString("target-arch")
+	targetArchStr, err := flags.GetString("target-arch")
 	check(err)
 
+	targetArch, err := arch.FromString(targetArchStr)
+	check(err)
 	opts := &awscloud.UploaderOptions{
 		TargetArch: targetArch,
 	}

commit c30e3017dd45a3248ee55e9447fcf27c3eb7b68e

bib: fix AWS upload with empty target-arch
This commit fixes the issue that with an unset --target-arch
the code will try to convert an empty string to an arch.Arch
which then fails. This is a regression from
https://github.com/osbuild/bootc-image-builder/pull/1017
that was not caught.

Closes: https://github.com/osbuild/bootc-image-builder/issues/1029

bib/cmd/bootc-image-builder/main.go
@@ -386,9 +386,13 @@ func handleAWSFlags(cmd *cobra.Command) (cloud.Uploader, error) {
 	}
 
 	// check as many permission prerequisites as possible before starting
-	targetArch, err := arch.FromString(targetArchStr)
-	if err != nil {
-		return nil, err
+	targetArch := arch.Current()
+	if targetArchStr != "" {
+		var err error
+		targetArch, err = arch.FromString(targetArchStr)
+		if err != nil {
+			return nil, err
+		}
 	}
 	uploaderOpts := &awscloud.UploaderOptions{
 		TargetArch: targetArch,

bib/cmd/upload/main.go
@@ -33,8 +33,12 @@ func uploadAMI(cmd *cobra.Command, args []string) {
 	targetArchStr, err := flags.GetString("target-arch")
 	check(err)
 
-	targetArch, err := arch.FromString(targetArchStr)
-	check(err)
+	targetArch := arch.Current()
+	if targetArchStr != "" {
+		var err error
+		targetArch, err = arch.FromString(targetArchStr)
+		check(err)
+	}
 	opts := &awscloud.UploaderOptions{
 		TargetArch: targetArch,
 	}