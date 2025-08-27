d090739433ec1124eee026a589e28ac182ecf26f
Michael Vogt <michael.vogt@gmail.com>
2025-08-27 10:52:06 +0200

    main: add regression test for aws upload target arch error
    This is a regression test to ensure the commandline handling
    of aws upload is correct. Its a followup for
    https://github.com/osbuild/bootc-image-builder/pull/1030

--- bib/cmd/bootc-image-builder/main_test.go
+++ bib/cmd/bootc-image-builder/main_test.go
@@ -1,9 +1,11 @@
 package main_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"testing"
@@ -17,6 +19,8 @@ import (
 	"github.com/osbuild/blueprint/pkg/blueprint"
 	"github.com/osbuild/images/pkg/arch"
 	"github.com/osbuild/images/pkg/bib/osinfo"
+	"github.com/osbuild/images/pkg/cloud"
+	"github.com/osbuild/images/pkg/cloud/awscloud"
 	"github.com/osbuild/images/pkg/container"
 	"github.com/osbuild/images/pkg/dnfjson"
 	"github.com/osbuild/images/pkg/manifest"
@@ -629,5 +633,70 @@ func TestCobraCmdlineVerbose(t *testing.T) {
 			assert.Equal(t, tc.expectedProgress, progressFlag)
 			assert.Equal(t, tc.expectedLogrusLevel, logrus.GetLevel())
 		})
+	}
+}
+
+type fakeAwsUploader struct {
+	checkCalls int
+
+	region, bucket, ami string
+	opts                *awscloud.UploaderOptions
+
+	uploadAndRegisterRead  bytes.Buffer
+	uploadAndRegisterCalls int
+	uploadAndRegisterErr   error
+}
+
+var _ = cloud.Uploader(&fakeAwsUploader{})
+
+func (fa *fakeAwsUploader) Check(status io.Writer) error {
+	fa.checkCalls++
+	return nil
+}
+
+func (fa *fakeAwsUploader) UploadAndRegister(r io.Reader, status io.Writer) error {
+	fa.uploadAndRegisterCalls++
+	_, err := io.Copy(&fa.uploadAndRegisterRead, r)
+	if err != nil {
+		panic(err)
+	}
+	return fa.uploadAndRegisterErr
+}
+
+func TestHandleAWSFlags(t *testing.T) {
+	for _, tc := range []struct {
+		extraArgs    []string
+		expectedOpts *awscloud.UploaderOptions
+	}{
+		{nil, &awscloud.UploaderOptions{TargetArch: arch.Current()}},
+		{[]string{"--target-arch=aarch64"}, &awscloud.UploaderOptions{TargetArch: arch.ARCH_AARCH64}},
+	} {
+		var fau fakeAwsUploader
+		t.Cleanup(main.MockAwscloudNewUploader(func(region string, bucket string, ami string, opts *awscloud.UploaderOptions) (cloud.Uploader, error) {
+			fau.region = region
+			fau.bucket = bucket
+			fau.ami = ami
+			fau.opts = opts
+			return &fau, nil
+		}))
+
+		rootCmd, err := main.BuildCobraCmdline()
+		assert.NoError(t, err)
+		// Commands() returns commandsordered by name
+		buildCmd := rootCmd.Commands()[0]
+		assert.Equal(t, "build", buildCmd.Name())
+		err = buildCmd.ParseFlags(append([]string{
+			"--aws-bucket=aws-bucket",
+			"--aws-ami-name=aws-ami-name",
+			"--aws-region=aws-region",
+			"--type=ami",
+		}, tc.extraArgs...))
+		assert.NoError(t, err)
+
+		uploader, err := main.HandleAWSFlags(buildCmd)
+		assert.NoError(t, err)
+		assert.NotNil(t, uploader)
+		assert.Equal(t, 1, fau.checkCalls)
+		assert.Equal(t, tc.expectedOpts, fau.opts)
 	}
 }
