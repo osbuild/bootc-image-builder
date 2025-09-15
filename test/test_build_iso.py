fb14f1b898f09a76116623945ccd7e515359968a
Michael Vogt <michael.vogt@gmail.com>
2025-09-15 17:31:52 +0200

    test: build anaconda bootc base container
    Build an anaconda bootc base container with a custom
    LiveOS and a anaconda enable kernel via an extra dracut
    run.

--- test/test_build_iso.py
+++ test/test_build_iso.py
@@ -1,12 +1,15 @@
 import os
+import json
 import platform
 import subprocess
 from contextlib import ExitStack
+import textwrap
 
 import pytest
 # local test utils
 import testutil
 from containerbuild import build_container_fixture    # pylint: disable=unused-import
+from containerbuild import make_container
 from testcases import gen_testcases
 from vm import QEMU
 
@@ -83,3 +86,81 @@ def test_iso_install_img_is_squashfs(tmp_path, image_type):
         # was an intermediate ext4 image "squashfs-root/LiveOS/rootfs.img"
         output = subprocess.check_output(["unsquashfs", "-ls", mount_point / "images/install.img"], text=True)
         assert "usr/bin/bootc" in output
+
+
+@pytest.mark.skipif(platform.system() != "Linux", reason="boot test only runs on linux right now")
+def test_container_iso_installs(tmp_path, build_container):
+    container_ref = "quay.io/centos-bootc/centos-bootc:stream9"
+
+    # XXX: this is not working
+    stage2 = "hd:/dev/sda4"
+    cfg = {
+        "customizations": {
+            "kernel": {
+                "append": f"systemd.debug-shell=1 rd.systemd.debug-shell=1 inst.stage2={stage2}:/sysroot/sysroot inst.debug",
+            },
+        },
+    }
+    config_json_path = tmp_path / "config.json"
+    config_json_path.write_text(json.dumps(cfg), encoding="utf-8")
+
+    # create anaconda iso from base
+    cntf_path = tmp_path / "Containerfile"
+    cntf_path.write_text(textwrap.dedent(f"""\n
+    FROM {container_ref}
+    RUN dnf install -y \
+         anaconda \
+         anaconda-install-env-deps \
+         anaconda-dracut \
+         dracut-config-generic \
+         dracut-network \
+         net-tools \
+         squashfs-tools \
+         && dnf clean all
+    # we need anacondoa in our initrd
+    RUN dracut -f -v --no-host-only --add anaconda \
+        /lib/modules/*/initramfs.img \
+        $(basename /usr/lib/modules/*)
+    # anaconda can only boot into a squshfs so we need the non-ostree
+    # version ready as a squashfs
+    # sucks, doubles size (XXX: remove all packages again? use build-container?)
+    #
+    # XXX: /LiveOS lives in /dev/sda4:/ostree/deploy/default/deploy/e63.../
+    RUN mkdir /LiveOS
+    RUN mkdir -p /tmp/liveos/{{etc,var,root,tmp,home,run,proc,sys,dev,usr}}
+    RUN chmod 1777 /tmp/liveos/tmp
+    RUN mksquashfs /usr /LiveOS/squashfs.img
+    RUN mksquashfs /tmp/liveos /LiveOS/squashfs.img -info -root-becomes usr
+    """), encoding="utf8")
+
+    output_path = tmp_path / "output"
+    output_path.mkdir()
+    with make_container(tmp_path) as container_tag:
+        cmd = [
+            *testutil.podman_run_common,
+            "-v", f"{config_json_path}:/config.json:ro",
+            "-v", f"{output_path}:/output",
+            "-v", "/var/tmp/osbuild-test-store:/store",  # share the cache between builds
+            "-v", "/var/lib/containers/storage:/var/lib/containers/storage",
+            build_container,
+            "--type", "raw",
+            f"localhost/{container_tag}",
+        ]
+        print(" ".join(cmd))
+        subprocess.check_call(cmd)
+        installer_iso_path = output_path / "image" / "disk.raw"
+
+        test_disk_path = installer_iso_path.with_name("test-disk.img")
+        with open(test_disk_path, "w", encoding="utf8") as fp:
+            fp.truncate(10_1000_1000_1000)
+        # install to test disk
+        #with QEMU(test_disk_path, cdrom=installer_iso_path) as vm:
+        with QEMU(installer_iso_path) as vm:
+            vm.start(wait_event="qmp:RESET", snapshot=False, use_ovmf=True)
+            vm.force_stop()
+        # boot test disk and do extremly simple check
+        with QEMU(test_disk_path) as vm:
+            vm.start(use_ovmf=True)
+            exit_status, _ = vm.run("true", user=image_type.username, password=image_type.password)
+            assert exit_status == 0
+            assert_kernel_args(vm, image_type)
