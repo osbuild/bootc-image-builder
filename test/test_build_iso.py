dff881b232455271530ea6bdf2d7151a713b6048
Michael Vogt <michael.vogt@gmail.com>
2025-09-24 19:20:07 +0200

    test: add links to the fixes for the remaining quirks

--- test/test_build_iso.py
+++ test/test_build_iso.py
@@ -158,12 +158,18 @@ def test_container_iso_installs(tmp_path, build_container, container_ref):
          && dnf clean all
     # shim-x64 is marked installed but the files are not in the expected
     # place for https://github.com/osbuild/osbuild/blob/v160/stages/org.osbuild.grub2.iso#L91, see
-    # https://github.com/osbuild/osbuild/pull/2202 for proper fix
     # workaround via reinstall, we could add a config to the grub2.iso
-    # stage to allow a different prefix that then would be used by anaconda
+    # stage to allow a different prefix that then would be used by
+    # anaconda.
+    # once https://github.com/osbuild/osbuild/pull/2202 is merged we
+    # can update images/ to set the correct efi_src_dir and this can
+    # be removed
     RUN dnf reinstall -y shim-x64
     # lorax wants to create a symlink in /mnt which points to /var/mnt
-    # on bootc but /var/mnt does not exist on some images
+    # on bootc but /var/mnt does not exist on some images.
+    #
+    # If https://gitlab.com/fedora/bootc/base-images/-/merge_requests/294
+    # gets merged this will be no longer needed
     RUN mkdir /var/mnt
     """), encoding="utf8")
 
